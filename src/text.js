6e3983327a1b98c27f15a266ddcfcf7974da4cca
karinka <kharinakaryna@gmail.com>
2020-04-20 22:56:29 +0300

    text for input

---
+++ src/text.js
@@ -0,0 +1,68 @@
+export const text = "In this book, we invite you join us on a whirlwind ride through ten years of laughing, " +
+    "playing, working, and learning together. We think it's important for you to realize how these ideas came " +
+    "together and to know that they have been refined through experiences with real students in real classrooms. " +
+    "The ideas in this book come, for the most part, out of our explorations together as we tried to integrate " +
+    "what we know about good literacy practices with what we know about vocabulary learning. A group of us, " +
+    "including the three authors, were part of a teacher research project with a focus on vocabulary that met " +
+    "for dinners, discussion, and brainstorming sessions for almost a decade. These meetings were part of what " +
+    "we call a think tank model of professional development and exploration (Henry et al., 1999). In this model, " +
+    "teachers and researchers come together, each bringing a high level of expertise in his or her own area. " +
+    "The combined expertise in our meeting room was amazing. Bonnie had taught for 30 years, served as the social " +
+    "studies helping teacher for her district, and, as a faculty associate (on loan from her district to supervise " +
+    "student teachers), taught the language arts methodology course for Simon Fraser University. At the time of " +
+    "this project, Bonnie was both a grade 6/7 teacher and the principal of an urban elementary school where " +
+    "students came from a variety of cultures and spoke several different languages. Jan had been a teacher " +
+    "for 23 years, the reading consultant for a large urban metropolitan district in Ontario, and a faculty " +
+    "associate who taught both the reading and the language arts methodology courses for Simon Fraser University. " +
+    "She was a grade 4/5 teacher at the time of the project and later became the primary literacy consultant " +
+    "for the Vancouver School Board. Both Bonnie and Jan had taught several different grades, from primary to " +
+    "intermediate, throughout their careers. Judy was an assistant and later associate professor in reading and " +
+    "literacy at Simon Fraser University after finishing her dissertation in educational psychology/curriculum and " +
+    "instruction/ reading/vocabulary at the University of Illinois at Urbana-Champaign. Prior to that she had been " +
+    "a teacher and graduated with a Reading Specialist credential from the University of California at Davis. " +
+    "Other core members of the group mentioned throughout the book were a band of exemplary classroom teachers: " +
+    "Teresa Blackstone, Alan Jones, and Susie Cross. Cindy Butler was the main behind-the-scenes research assistant " +
+    "for the project, and she entered the teaching profession during this project. Our common connection came " +
+    "through Simon Fraser University, where several members of the group had served as faculty associates, took " +
+    "classes, or taught classes on literacy. From the outset, it was clear that the members of the vocabulary " +
+    "group shared a common vision and theoretical frame. In particular, we were informed by Lev Vygotsky's notion " +
+    "of sociocultural interactions and the power of discussion as a means to create new understandings and expand " +
+    "our horizons (Vygotsky, 1978). When Judy received a grant to explore alternatives to traditional vocabulary " +
+    "instruction, she invited members of the group to join her as teacher researchers who combined their expertise " +
+    "in teaching with her knowledge of vocabulary research. The diversity of perspectives was celebrated as " +
+    "essential in their construction of new understandings about effective vocabulary instruction. Coming together " +
+    "as a group of experts alleviated many of the issues associated with collaborative projects, such as hierarchy, " +
+    "credibility, and ownership (Henry et al., 1999). Because their expertise as teachers brought them to the " +
+    "group, the teachers didn't feel as though they were unequal partners. Instead, their expertise was explicitly " +
+    "valued and encouraged to flourish. The university participants were also acknowledged as experts in the " +
+    "research community who could offer a different set of skills and abilities to the group. Our unique strengths " +
+    "contributed to our abilities to learn from each other, help each other, reflect on our practices, engage in " +
+    "shared critique of those practices, and support one another in making professional choices and change. In one " +
+    "paper written about this project, we identif1ed these elements of the group dynamic as essential in c" +
+    "ontributing to the sense of community: (a) safety, built on respect, (b) engaging dialogue, (c) collaboration " +
+    "among equals, (d) personal commitment, and (e) time. The topic, vocabulary learning, was defined, but how we " +
+    "explored it together and what we were not. The teacher researchers and university researchers all acknowledge " +
+    "that they changed and benefited from their involvement in the project (Henry et al., 1999). We were all " +
+    "energized by the opportunity to discuss ideas, practice, and problems with each other. Bonnie gave a workshop " +
+    "for a local school, the principal, Grant Mcintosh, thanked her by presenting her with this story: She was " +
+    "arriving at school feeling bored and listless. The spectre of another morning reading her students' stories, " +
+    "dripping with mindless violence and populated by mundane characters, was daunting. She needed a teaching " +
+    "makeover desperately! She yearned to hear real children's voices in their writing, real emotion, real people, " +
+    "the imagination that kids have. Where could she go? It wasn't as if there was a shop like a beauty salon " +
+    "where one can go for this kind of service. Mud mask? Hair color? Nails? Style? Massage? New way of teaching " +
+    "writing? Just when her life was at its darkest, when she was considering a career in real estate, she got a " +
+    "call that was to change her life-well, her teaching life, anyway. She joined a group of teachers who shared " +
+    "some of the same feelings about kids' writing. They talked about it, they laughed, they cried, and they " +
+    "looked for ways to help children tap into their rich imaginations. She brought real stories with powerful " +
+    "language to the students and let them borrow and steal words and characters and ideas for their writing. " +
+    "She developed all kinds of strategies to help kids discover, enjoy, and play with language. They wrote poems, " +
+    "memories, adventure stories, and picture books. They wrote like readers and read like writers! It was like " +
+    "a renaissance in her classroom. Now every day she rushes to school, flushed with excitement at the prospect " +
+    "of reading her children's stories and seeing the fruits of her labors. Right? Get real! But at least now there " +
+    "is a possibility that she will be able to read some writing that is fresh, interesting, dynamic, imaginative. " +
+    "She'll settle for just one of these. This humorous essay shows why we did what we did. In the workshop, " +
+    "Bonnie conveyed her previous sense of despair as a teacher of writing and the excitement she felt as her " +
+    "students began to become conscious of the power of words as tools of communication. Many teachers share her " +
+    "struggle. Perhaps you are one of them. If so, we hope our explanations of struggles and successes will enable " +
+    "you to ponder your own practice and help you with the vital job of teaching students to be better readers, " +
+    "writers, and word users.";
