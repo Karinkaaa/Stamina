cc246958a6e44749e3f97a1260d1aab9d280f569
karyna <9548026git*>
2021-10-27 15:46:03 +0300

    updated timer methods

--- src/timerView/Timer.js
+++ src/timerView/Timer.js
@@ -1,37 +1,44 @@
 import { getMinutesAndSeconds, getMinutesInStringFormat, getSecondsInStringFormat } from "../utils/methods";
+import { TIMER_STATE_IDLE, TIMER_STATE_START, TIMER_STATE_STOP } from "../utils/constants";
 
 class Timer {
     constructor(cb) {
         this.begin = null;
         this.end = null;
         this.cb = cb;
+        this.state = TIMER_STATE_IDLE;
     }
 
     start() {
-        if (this.begin) return;
-        console.log("start");
-        this.begin = Date.now();
-        this.interval = setInterval(() => this.cb(), 1000);
+        if (this.state === TIMER_STATE_IDLE) {
+            this.state = TIMER_STATE_START;
+            this.begin = Date.now();
+            this.interval = setInterval(() => this.cb(), 1000);
+        }
     }
 
     stop() {
-        if (this.end) return;
-        console.log("stop");
-        this.end = Date.now();
-        clearInterval(this.interval);
+        if (this.state === TIMER_STATE_START) {
+            this.state = TIMER_STATE_STOP;
+            this.end = Date.now();
+            clearInterval(this.interval);
+        }
     }
 
     reset() {
-        console.log("reset");
+        this.state = TIMER_STATE_IDLE;
         this.begin = null;
         this.end = null;
         clearInterval(this.interval);
     }
 
     resume() {
-        this.begin = Date.now() - this.getTime();
-        this.end = null;
-        this.interval = setInterval(() => this.cb(), 1000);
+        if (this.state === TIMER_STATE_STOP) {
+            this.state = TIMER_STATE_START;
+            this.begin = Date.now() - this.getTime();
+            this.end = null;
+            this.interval = setInterval(() => this.cb(), 1000);
+        }
     }
 
     getTime() {
@@ -52,7 +59,7 @@ class Timer {
     };
 
     isStarted() {
-        return !!this.begin;
+        return this.state === TIMER_STATE_START;
     }
 }
 
