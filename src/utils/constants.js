e5979400721359b55d46da9fa576e5b7ab49a097
karyna <9548026git*>
2021-10-27 12:24:18 +0300

    added constants

---
+++ src/utils/constants.js
@@ -0,0 +1,15 @@
+export const STAMINA = "Stamina";
+export const ESCAPE = "Escape";
+export const SPACE = "Space";
+
+export const SPACE_START = "SPACE - START ";
+export const ESCAPE_PAUSE = " ESC - PAUSE / EXIT";
+
+export const TYPED_CHARS = "Typed chars";
+export const SPEED = "Speed";
+export const ACCURACY = "Accuracy";
+
+export const MIDNIGHT_TIME = "00 : 00";
+export const SIGNS = " signs";
+export const SIGNS_PER_MINUTE = " signs/minute";
+export const PERCENT_SIGN = "%";
