9a8be59388492d64d3f447d1192d082a91ef5af4
karyna <9548026git*>
2021-11-12 13:29:17 +0200

    refactoring

--- src/utils/text.js
+++ src/utils/text.js
@@ -65,4 +65,153 @@ export const text = "In this book, we invite you join us on a whirlwind ride thr
     "students began to become conscious of the power of words as tools of communication. Many teachers share her " +
     "struggle. Perhaps you are one of them. If so, we hope our explanations of struggles and successes will enable " +
     "you to ponder your own practice and help you with the vital job of teaching students to be better readers, " +
-    "writers, and word users.";
+    "writers, and word users.In this book, we invite you join us on a whirlwind ride through ten years of laughing, " +
+    "playing, working, and learning together. We think it's important for you to realize how these ideas came " +
+    "together and to know that they have been refined through experiences with real students in real classrooms. " +
+    "The ideas in this book come, for the most part, out of our explorations together as we tried to integrate " +
+    "what we know about good literacy practices with what we know about vocabulary learning. A group of us, " +
+    "including the three authors, were part of a teacher research project with a focus on vocabulary that met " +
+    "for dinners, discussion, and brainstorming sessions for almost a decade. These meetings were part of what " +
+    "we call a think tank model of professional development and exploration (Henry et al., 1999). In this model, " +
+    "teachers and researchers come together, each bringing a high level of expertise in his or her own area. " +
+    "The combined expertise in our meeting room was amazing. Bonnie had taught for 30 years, served as the social " +
+    "studies helping teacher for her district, and, as a faculty associate (on loan from her district to supervise " +
+    "student teachers), taught the language arts methodology course for Simon Fraser University. At the time of " +
+    "this project, Bonnie was both a grade 6/7 teacher and the principal of an urban elementary school where " +
+    "students came from a variety of cultures and spoke several different languages. Jan had been a teacher " +
+    "for 23 years, the reading consultant for a large urban metropolitan district in Ontario, and a faculty " +
+    "associate who taught both the reading and the language arts methodology courses for Simon Fraser University. " +
+    "She was a grade 4/5 teacher at the time of the project and later became the primary literacy consultant " +
+    "for the Vancouver School Board. Both Bonnie and Jan had taught several different grades, from primary to " +
+    "intermediate, throughout their careers. Judy was an assistant and later associate professor in reading and " +
+    "literacy at Simon Fraser University after finishing her dissertation in educational psychology/curriculum and " +
+    "instruction/ reading/vocabulary at the University of Illinois at Urbana-Champaign. Prior to that she had been " +
+    "a teacher and graduated with a Reading Specialist credential from the University of California at Davis. " +
+    "Other core members of the group mentioned throughout the book were a band of exemplary classroom teachers: " +
+    "Teresa Blackstone, Alan Jones, and Susie Cross. Cindy Butler was the main behind-the-scenes research assistant " +
+    "for the project, and she entered the teaching profession during this project. Our common connection came " +
+    "through Simon Fraser University, where several members of the group had served as faculty associates, took " +
+    "classes, or taught classes on literacy. From the outset, it was clear that the members of the vocabulary " +
+    "group shared a common vision and theoretical frame. In particular, we were informed by Lev Vygotsky's notion " +
+    "of sociocultural interactions and the power of discussion as a means to create new understandings and expand " +
+    "our horizons (Vygotsky, 1978). When Judy received a grant to explore alternatives to traditional vocabulary " +
+    "instruction, she invited members of the group to join her as teacher researchers who combined their expertise " +
+    "in teaching with her knowledge of vocabulary research. The diversity of perspectives was celebrated as " +
+    "essential in their construction of new understandings about effective vocabulary instruction. Coming together " +
+    "as a group of experts alleviated many of the issues associated with collaborative projects, such as hierarchy, " +
+    "credibility, and ownership (Henry et al., 1999). Because their expertise as teachers brought them to the " +
+    "group, the teachers didn'stories feel as though they were unequal partners. Instead, their expertise was " +
+    "explicitly valued and encouraged to flourish. The university participants were also acknowledged as experts in " +
+    "the research community who could offer a different set of skills and abilities to the group. Our unique " +
+    "strengths contributed to our abilities to learn from each other, help each other, reflect on our practices, " +
+    "engage in shared critique of those practices, and support one another in making professional choices and " +
+    "change. In one paper written about this project, we identif1ed these elements of the group dynamic as essential " +
+    "in contributing to the sense of community: (a) safety, built on respect, (b) engaging dialogue, (c) collaboration " +
+    "among equals, (d) personal commitment, and (e) time. The topic, vocabulary learning, was defined, but how we " +
+    "explored it together and what we were not. The teacher researchers and university researchers all acknowledge " +
+    "that they changed and benefited from their involvement in the project (Henry et al., 1999). We were all " +
+    "energized by the opportunity to discuss ideas, practice, and problems with each other. Bonnie gave a workshop " +
+    "for a local school, the principal, Grant Mcintosh, thanked her by presenting her with this story: She was " +
+    "arriving at school feeling bored and listless. The spectre of another morning reading her students' stories, " +
+    "dripping with mindless violence and populated by mundane characters, was daunting. She needed a teaching " +
+    "makeover desperately! She yearned to hear real children's voices in their writing, real emotion, real people, " +
+    "the imagination that kids have. Where could she go? It wasn'stories as if there was a shop like a beauty salon " +
+    "where one can go for this kind of service. Mud mask? Hair color? Nails? Style? Massage? New way of teaching " +
+    "writing? Just when her life was at its darkest, when she was considering a career in real estate, she got a " +
+    "call that was to change her life-well, her teaching life, anyway. She joined a group of teachers who shared " +
+    "some of the same feelings about kids' writing. They talked about it, they laughed, they cried, and they " +
+    "looked for ways to help children tap into their rich imaginations. She brought real stories with powerful " +
+    "language to the students and let them borrow and steal words and characters and ideas for their writing. " +
+    "She developed all kinds of strategies to help kids discover, enjoy, and play with language. They wrote poems, " +
+    "memories, adventure stories, and picture books. They wrote like readers and read like writers! It was like " +
+    "a renaissance in her classroom. Now every day she rushes to school, flushed with excitement at the prospect " +
+    "of reading her children's stories and seeing the fruits of her labors. Right? Get real! But at least now there " +
+    "is a possibility that she will be able to read some writing that is fresh, interesting, dynamic, imaginative. " +
+    "She'll settle for just one of these. This humorous essay shows why we did what we did. In the workshop, " +
+    "Bonnie conveyed her previous sense of despair as a teacher of writing and the excitement she felt as her " +
+    "students began to become conscious of the power of words as tools of communication. Many teachers share her " +
+    "struggle. Perhaps you are one of them. If so, we hope our explanations of struggles and successes will enable " +
+    "you to ponder your own practice and help you with the vital job of teaching students to be better readers, " +
+    "writers, and word users. If you read this story you'll be able to find a common language with any American. All " +
+    "of them are very proud of their Constitution. The USA Constitution was written by fifty-five men who met at " +
+    "Philadelphia Convention in 1787. In four months they wrote the Constitution which has lasted over 200 years!" +
+    "All were white men. The average age was forty-two. Many of these men had been leaders during the American " +
+    "Revolution. About three-fourths of them had served in Congress. Most were leaders in their states. Some were " +
+    "rich, but most were not. None were poor. There were no native Americans among the delegates. There were no " +
+    "women. There were no black men or slaves. Poor farmers were not present either. George Washington, James " +
+    "Madison, and Benjamin Franklin were the three important delegates to the Convention. George Washington came " +
+    "from Virginia. He was probably the most respected man in the country. As the commander-in-chief of the " +
+    "American army during the Revolution, he was a great hero to most people. Then he had retired to his plantation " +
+    "and would have liked to remain there. However, his friends told him he should attend the convention. They said " +
+    "his support was necessary to get a new constitution accepted by the people. Since Washington thought a stronger " +
+    "national government was necessary, he came to Philadelphia. James Madison is often called the \"Father of the " +
+    "Constitution\". His ideas about government greatly influenced the other delegates. He had already developed a " +
+    "written plan for the new government which he brought to Philadelphia. It was known as the Virginia plan and it " +
+    "called for a strong national government. Madison took notes during the meetings. Much of what we know about the " +
+    "Philadelphia Convention is based on his notes. Benjamin Franklin attended the convention as a delegate from " +
+    "Pennsylvania. He was 81 years old and in poor health. Like Washington, he was highly respected by the " +
+    "Americans. He had been a printer, inventor and writer. He had also helped the country develop good relations " +
+    "with other nations. At the convention, he encouraged the delegates to cooperate with each other and work hard " +
+    "to settle their differences. His support of the Constitution was important to the other delegates. Once the " +
+    "Framers reached these agreements, it was time to get down to work and create a constitution. Delegates from " +
+    "states with large populations believed that a state with more people should have more votes in Congress. " +
+    "During the long debates, the Framers could not reach a decision on this issue. Neither side was willing to " +
+    "give in. The delegates were almost ready to quit and go home. A special committee of one delegate from each " +
+    "state was formed to try and find a solution. The members of the committee worked hard to find a compromise a " +
+    "majority of the delegates would accept. The result of the committee's work is known as the Great Compromise. " +
+    "The main parts of this compromise were: 1) Congress would have two parts, or houses. These houses would be the " +
+    "Senate and the House of Representatives. 2) Each state, large or small, would have 2 representatives in the " +
+    "Senate. 3) In the House, the number of representatives of each state would be based on the number of people " +
+    "living in that state. By May 25, 1787, the delegates from over half of the states had arrived in Philadelphia. " +
+    "From the start, the Framers agreed on 4 things: 1. George Washington would serve as president of the " +
+    "convention. 2. Each state, large or small, would have one vote at the convention. 3. They would not follow the " +
+    "instructions Congress had given them. They would not even try to improve the Articles of confederation. They " +
+    "thought the Articles had too many weaknesses. Instead, they decided to write an entirely new constitution. " +
+    "4. They wo.uld keep their discussions private. They also decided that what they said at the convention would " +
+    "remain a secret for 30 years. There were 2 reasons for this: 1) They believed they needed to speak freely to " +
+    "create the best possible constitution. If people could listen to them and tell others what they said, they " +
+    "would not feel as free to discuss their ideas. 2) They wanted the new constitution to be accepted by the " +
+    "people. They were afraid that the people might not accept it if they knew all the disagreements the Framers " +
+    "had. The agreement meant that each state would have equal power in the Senate. The states with more people " +
+    "would have more power in the House of Representatives. However, no law could be passed unless a majority of " +
+    "both the Senate and the House of Representatives voted for it. Because of this compromise large and small " +
+    "states could check each other's power and protect their own interests. When it comes to American newspapers, " +
+    "a lot of people outside the United States think of  that slender, serious paper, the International Herald " +
+    "Tribune, said to be on the daily reading list of many world leaders. The Herald Tribune, however, is not " +
+    "really an American paper. It is published in Paris (and printed simultaneously in Paris, London, Zurich, " +
+    "Hongkong, Singapore, The Hague, Marseille, and Miami) as an international digest of news, most of it taken " +
+    "from its much-larger parents. The New York Times and The Washington Post. Many people in America have never " +
+    "heard of it. And few Americans would read it when they can get the real thing, that is, the full-sized daily " +
+    "newspapers. In 1986, a total of 9,144 newspapers (daily, Sunday, weekly, etc.) appeared in 6,516 towns in the " +
+    "United States. Most of the daily newspapers are published rain or shine, on Christmas, Thanksgiving, or the " +
+    "Fourth of July (Independence Day). Including the 85 foreign-language newspapers published in 34 different " +
+    "languages, the daily newspapers in the United States sell over 63 million copies a day. The 762 Sunday papers " +
+    "are usually much larger than the regular editions. The record for a Sunday paper is held by The New York Times. " +
+    "One issue on a Sunday in 1965 contained 946 pages, weighed 36 pounds, and cost 50 cents. Reading the Sunday " +
+    "paper is an American tradition, for some people an alternative to going to church. Getting through all of the " +
+    "sections can take most of the day, leaving just enough time for the leisurely Sunday dinner. The Sunday " +
+    "newspapers have an average circulation of 57 million copies. There are also more than 7,000 newspapers which " +
+    "are published weekly, semi-weekly or monthly. Most daily newspapers are of the \"quality\" rather than the " +
+    "\"popular\" (that is, non-quality) variety. Among the twenty newspapers with the largest circulation only two " +
+    "or three regularly feature crime, sex, and scandal. The paper with the largest circulation, The Wall Street " +
+    "Journal, is a very serious newspaper indeed. It is often said that there is no \"national press\" in the " +
+    "United States as there is in Great Britain, for instance, where five popular followed by three quality " +
+    "newspapers dominate the circulation figures and are read nationwide. In one sense this is true. Most daily " +
+    "newspapers are distributed locally, or regionally, people buying one of the big city newspapers in addition to " +
+    "the smaller local ones. A few of the best-known newspapers such as The Wall Street Journal can be found " +
+    "throughout the country. Yet, one wouldn'stories expect The Milwaukee Journal to be read: in Boston, or The " +
+    "Boston Globe in Houston. There has been one attempt to publish a truly national newspaper, USA: Today. But it " +
+    "still has only a circulation of 1.2 million and, in its popular form, can only offer news of general interest. " +
+    "This is not enough in a country where state, city, and local news and political developments most deeply affect " +
+    "readers and are therefore especially interesting to them. In another sense, however, there is a national press, " +
+    "one that comes from influence and the sharing of news. Some of the largest newspapers are at the same time " +
+    "news-gathering businesses. They not only print newspapers, they also collect and sell news, news features, and " +
+    "photographs to hundreds of other papers in the U.S. and abroad. Three of the better-known of these are The New " +
+    "York Times', The Washington Post's, and the Los Angeles Times' news services. In one famous example, an expose " +
+    "of the CIA published in The New York Times also appeared in 400 other American papers and was picked up or " +
+    "used in some way by hundreds more overseas. \"Picked up\" is not quite right. Such stories are copyrighted and " +
+    "other newspapers must pay for their use. Often newspapers try to avoid paying for this news by using the " +
+    "original newspaper's story and quoting the story indirectly (\"The Washington Post reported today that...\"). " +
+    "Because so many other newspapers print (or \"borrow\") news stories from the major American newspapers and " +
+    "magazines, they have great national and international influence. This influence spreads far beyond their own " +
+    "readers.";
